f6facabd09b195c1fb52a486a95085f52c4fb52c
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-22 22:13:05 +0530

    handle multiple PINGs

--- app/main.js
+++ app/main.js
@@ -6,6 +6,18 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    const connections = data.toString().split("PING");
+
+    console.log(connections);
+
+    let res = "";
+    Array.from({ length: connections.length - 1 }).forEach((_, index) => {
+      res += "+PONG\r\n";
+    });
+
+    connection.write(res);
+  });
 });
 
 server.listen(6379, "127.0.0.1", () => {
